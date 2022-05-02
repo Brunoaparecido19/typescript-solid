725cfa57df3abd0f75607a0edb751fcabf728c45
Brunoaparecido19 <ap24201@icloud.com>
2022-05-02 17:00:47 -0300

    Open/Closed Principle (Princípio do aberto/fechado) #1

---
+++ src/ocp/main.ts
@@ -0,0 +1,26 @@
+/*
+Open/closed principle
+Entidades devem estar abertas para extensão, mas fechadas para modificação.
+ */
+import { Messaging } from './services/messaging';
+import { Order } from './classes/order';
+import { Persistency } from './services/persistency';
+import { Product } from './classes/product';
+import { ShoppingCart } from './classes/shopping-cart';
+import { NoDiscount } from './classes/discount';
+
+// const temPercentDiscount = new TemPercentDiscount();
+const noDiscount = new NoDiscount();
+const shoppingCart = new ShoppingCart(noDiscount);
+const messaging = new Messaging();
+const persistency = new Persistency();
+const order = new Order(shoppingCart, messaging, persistency);
+
+shoppingCart.addItem(new Product('Camisa', 50));
+shoppingCart.addItem(new Product('Caderno', 15.98));
+shoppingCart.addItem(new Product('Calça', 28.5));
+
+console.log(shoppingCart.totalWithDicount());
+console.log(shoppingCart.items);
+order.checkout();
+console.log(order.orderStatus);
